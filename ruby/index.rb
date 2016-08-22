d40ebcce5eeccf8d0f07d40bfd3d0f09dd37ed7f
bingohuang <me@bingohuang.com>
2016-08-22 14:51:12 +0800

    Fix ruby output

--- ruby/index.rb
+++ ruby/index.rb
@@ -4,7 +4,7 @@ print "begin to start server,open port (2000)"
 loop do
 client = server.accept
 client.puts "The connection is now in place"
-client.puts "The time is #(Time.now)"
+client.puts "The time is #{Time.now}"
 client.puts "The connection is now closed. Thank you for connecting"
 client.close
 end
